9cd95c39807ce10ddf86dc273d8eecc67f4c4230
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-10 12:25:28 +0100

    aggiungi classe per gestire i template

---
+++ php/template-handler.php
@@ -0,0 +1,67 @@
+<?php
+class TemplateHandler
+{
+    private $page;
+    private $root;
+
+    public function __construct(string $root, string $standard)
+    {
+        if ($standard != "xhtml" && $standard != "html5") {
+            throw new UnexpectedValueException("Unrecognized standard declared", 1);
+        }
+
+        $this->root = $root;
+        $this->page = str_replace("<rootFolder />", $root, file_get_contents(__DIR__ . "/components/" . $standard . "-template.php"));
+    }
+
+    public function setTitle(string $title)
+    {
+        $this->page = str_replace("<titlePlaceholder />", $title, $this->page);
+    }
+
+    public function setDescription(string $description)
+    {
+        $this->page = str_replace("<descriptionPlaceholder />", $description, $this->page);
+    }
+
+    public function setKeywords(string $keywords)
+    {
+        $this->page = str_replace("<keywordsPlaceholder />", $keywords, $this->page);
+    }
+
+    public function setAuthor(string $author)
+    {
+        $this->page = str_replace("<authorPlaceholder />", $author, $this->page);
+
+    }
+
+    public function setLogin(string $loginSnippet)
+    {
+        $this->page = str_replace("<loginPlaceholder />", $loginSnippet, $this->page);
+    }
+
+    public function setNav(string $navSnippet)
+    {
+        $this->page = str_replace("<navPlaceholder />", $navSnippet, $this->page);
+    }
+
+    public function setBreadcrumb(string $breadcrumbSnippet)
+    {
+        $this->page = str_replace("<breadcrumbPlaceholder />", $breadcrumbSnippet, $this->page);
+    }
+
+    public function setContent(string $contentSnippet)
+    {
+        $this->page = str_replace("<contentPlaceholder />", $contentSnippet, $this->page);
+    }
+
+    public function send()
+    {
+        echo $this->page;
+    }
+
+    public function save(string $file)
+    {
+        file_put_contents($file, $this->page);
+    }
+}
