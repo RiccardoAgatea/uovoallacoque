b94f4a62f39d11121ec414201d9cefdf634d8c47
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-10 11:36:05 +0100

    aggiungi template xhtml

---
+++ php/components/xhtml-template.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html
+          PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xml:lang="it"
+      lang="it">
+
+    <head>
+        <title>
+            <titlePlaceholder />
+        </title>
+        <meta name="title"
+              content="<titlePlaceholder />" />
+        <meta name="description"
+              content="<descriptionPlaceholder />" />
+        <meta name="keywords"
+              content="<keywordsPlaceholder />" />
+        <meta name="language"
+              content="italian it" />
+        <meta name="author"
+              content="<authorPlaceholder />" />
+        <meta http-equiv="Content-type"
+              content="text/html; charset=utf-8" />
+        <link rel="icon"
+              href="<rootFolder />/img/logo/logo-head.jpg"
+              type="image/x-icon" />
+        <link rel="stylesheet"
+              media="screen"
+              href="<rootFolder />/styles/screen.css"
+              type="text/css" />
+        <link rel="stylesheet"
+              media="print"
+              href="<rootFolder />/prove/print.css"
+              type="text/css" />
+    </head>
+
+    <body>
+        <div id="header">
+            <loginPlaceholder />
+        </div>
+        <navPlaceholder />
+        <breadcrumbPlaceholder />
+        <contentPlaceholder />
+        <div id="footer">
+            <ul id="nomi-contatti">
+                <li>Agatea Riccardo</li>
+                <li>Bosinceanu Ecaterina</li>
+                <li>Righetto Sara</li>
+                <li>Schiavon Rebecca</li>
+            </ul>
+            <img class="immagine-footer"
+                 id="immagine-html"
+                 src="<rootFolder />/img/validHTML.png"
+                 alt="HTML valido" />
+            <img class="immagine-footer"
+                 id="immagine-css"
+                 src="<rootFolder />/img/validCSS.gif"
+                 alt="CSS valido" />
+        </div>
+    </body>
+
+</html>
