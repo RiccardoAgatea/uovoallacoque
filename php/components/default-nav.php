75c318cb841c8569583dc9e07ac5e93cde629308
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-10 16:04:50 +0100

    aggiungi snippet per menu default

---
+++ php/components/default-nav.php
@@ -0,0 +1,7 @@
+<ul>
+    <li><a href="<rootFolder />/index.php">Home</a></li>
+    <li><a href="<rootFolder />/php/primi.php">Primi Piatti</a></li>
+    <li><a href="<rootFolder />/php/secondi.php">Secondi Piatti</a></li>
+    <li><a href="<rootFolder />/php/dolci.php">Dolci</a></li>
+    <li>Ricerca</li>
+</ul>
