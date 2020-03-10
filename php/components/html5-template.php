23a23a7fbba75a11b1ea1e3287e23d7d114f71dd
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-10 11:36:55 +0100

    aggiungi template html5

---
+++ php/components/html5-template.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml"
+      xml:lang="it"
+      lang="it">
+
+    <head>
+        <title>
+            <titlePlaceholder />
+        </title>
+        <meta name="title"
+              content="<titlePlaceholder />" />
+        <meta name="description"
+              content="<descriptionPlaceholder />" />
+        <meta name="keywords"
+              content="<keywordsPlaceholder />" />
+        <meta name="language"
+              content="italian it" />
+        <meta name="author"
+              content="<authorPlaceholder />" />
+        <meta charset="utf-8" />
+        <link rel="icon"
+              href="<rootFolder />/img/logo/logo-head.jpg"
+              type="image/x-icon" />
+        <link rel="stylesheet"
+              media="screen"
+              href="<rootFolder />/styles/screen.css" />
+        <link rel="stylesheet"
+              media="print"
+              href="<rootFolder />/prove/print.css" />
+    </head>
+
+    <body>
+        <header id="header">
+            <loginPlaceholder />
+        </header>
+        <navPlaceholder />
+        <breadcrumbPlaceholder />
+        <contentPlaceholder />
+        <footer id="footer">
+            <ul id="nomi-contatti">
+                <li>Agatea Riccardo</li>
+                <li>Bosinceanu Ecaterina</li>
+                <li>Righetto Sara</li>
+                <li>Schiavon Rebecca</li>
+            </ul>
+            <img class="immagine-footer"
+                 id="immagine-html"
+                 src="<rootFolder />/img/validHTML.png"
+                 alt="HTML valido" />
+            <img class="immagine-footer"
+                 id="immagine-css"
+                 src="<rootFolder />/img/validCSS.gif"
+                 alt="CSS valido" />
+        </footer>
+    </body>
+
+</html>
