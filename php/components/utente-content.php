f04525c2f1c9aec0922ac416a601b5b5ad5a0592
ecaterina-b <ecaterinabosinceanu@gmail.com>
2020-03-11 21:03:44 +0100

    caricate pagine ricetta-content e utente-content

---
+++ php/components/utente-content.php
@@ -0,0 +1,18 @@
+<div id="content">
+	<div class="foto-nome">
+        <img class="foto-utente" src="" alt=""/>
+        <p class="nome-utente"></p>
+    </div>
+    <div class="cambia-dati">
+        <form>
+            <label for="nuovo-nickname">Nuovo nickname</label>
+            <input id="nuovo-nickname" type="text" name="nuovo-nickname" />
+            <label for="nuova-password">Nuova password</label>
+            <input id="nuova-password" type="text" name="nuova-password" />
+            <label for="conferma-password">Conferma password</label>
+            <input id="conferma-password" type="text" name="conferma-password" />
+            <label for="nuova-immagine">Nuova immagine</label>
+            <input type="image" id="nuova-immagine" name="nuova-immagine" />
+          </form>
+    </div>
+</div>
