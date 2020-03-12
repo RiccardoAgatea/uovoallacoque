a2e52e30c97a6f92b2759f9edfb01eb547166a04
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-03-12 17:06:01 +0100

    close #7

---
+++ php/components/login-content.php
@@ -0,0 +1,22 @@
+<div id="content">
+    <h1>Accedi</h1>
+    <form action="" method="POST" id="form-login">
+        <fieldset class="form-fieldset">
+            <legend>Accedi</legend>
+            <ul class="form-container">
+                <li class="form-element">
+                    <label class="label" for="login-email">Email:</label>
+                    <input class="barra-input" id="login-email" type="email" name="email"/>
+                </li>
+                <li class="form-element">
+                    <label class="label" for="login-password">Password:</label>
+                    <input class="barra-input" id="login-password" type="password" name="password"/>
+                </li>
+                <li class="form-element">
+                    <input class="submit" type="submit" value="Accedi"/>
+                </li>
+            </ul>
+        </fieldset>
+    </form>
+    <p id="registrazione">Non sei ancora registrato? <a href="<rootFolder />/php/signup.php">Registrati</a></p>
+</div>
---
+++ php/login.php
@@ -0,0 +1,30 @@
+<?php
+require_once "./template-handler.php";
+
+$handler = new TemplateHandler("..", "xhtml");
+
+$handler->setTitle("Uovo alla Coque");
+$handler->setDescription("");
+$handler->setKeywords("");
+$handler->setAuthor("");
+
+$handler->setLogin(  
+    str_replace("<a href=\"<rootFolder />/php/login.php\">Accedi</a>", 
+        "Accedi", 
+        file_get_contents(__DIR__ . "/components/default-login.php")
+    )
+);
+
+$handler->setNav(
+    file_get_contents(__DIR__ . "/components/default-nav.php")
+);
+
+$handler->setBreadcrumb("Ti trovi in: Accedi");
+
+$content = file_get_contents(__DIR__ . "/components/login-content.php");
+
+$content = preg_replace("(<top.*Placeholder />)", "", $content);
+
+$handler->setContent($content);
+
+$handler->send();
