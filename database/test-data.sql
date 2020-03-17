ab31df85815ea874220cc15edfeb34f4c6387ff5
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-03-17 13:09:00 +0100

    aggiungi valori per utenti e voti

--- database/test-data.sql
+++ database/test-data.sql
@@ -28,3 +28,34 @@ VALUES
     "<rootFolder />/img/ricette/default.png",
     3
   );
+INSERT INTO utenti (email, passw, nickname)
+VALUES
+  (
+    "rosalinda.puccipucci@gmail.com",
+    "fragola",
+    "Tenebrina634"
+  ),
+  (
+    "assunta.incielo@libero.it",
+    "pentecoste",
+    "SacroCuoreDiMaria"
+  ),
+  (
+    "american.people@gmail.com",
+    "idiot",
+    "PastaAlSugoConLaPanna"
+  );
+INSERT INTO voti (utente, ricetta, voto)
+VALUES
+  (1, 1, 5),
+  (1, 2, 3),
+  (1, 3, 4),
+  (1, 4, 3),
+  (2, 1, 3),
+  (2, 2, 4),
+  (2, 3, 5),
+  (2, 4, 2),
+  (3, 1, 1),
+  (3, 2, 2),
+  (3, 3, 3),
+  (3, 4, 2);
