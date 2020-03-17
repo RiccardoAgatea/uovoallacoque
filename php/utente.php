b9ce768ceb27b9e6ecef36d58467ad9f71d27d04
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-03-17 11:06:23 +0100

    aggiunto ricetta.php e utente.php #5 #6

---
+++ php/utente.php
@@ -0,0 +1,27 @@
+<?php
+require_once "./template-handler.php";
+
+$handler = new TemplateHandler("..", "xhtml");
+
+$handler->setTitle("Uovo alla Coque");
+$handler->setDescription("");
+$handler->setKeywords("");
+$handler->setAuthor("");
+
+$handler->setLogin( 
+    file_get_contents(__DIR__ . "/components/default-login.php")  
+);
+
+$handler->setNav(
+    file_get_contents(__DIR__ . "/components/default-nav.php")
+);
+
+$handler->setBreadcrumb("Ti trovi in: Home &gt Primi");
+
+$content = file_get_contents(__DIR__ . "/components/utente-content.php");
+
+$content = preg_replace("(<top.*Placeholder />)", "", $content);
+
+$handler->setContent($content);
+
+$handler->send();
