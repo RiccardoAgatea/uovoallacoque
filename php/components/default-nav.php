caa79497a83ae44b64257b715b5dcf738611312d
ecaterina-b <ecaterinabosinceanu@gmail.com>
2020-03-17 16:32:28 +0100

    cominciato css e inseriti id utili in default-nav

--- php/components/default-nav.php
+++ php/components/default-nav.php
@@ -1,7 +1,9 @@
-<ul>
-    <li><a href="<rootFolder />/index.php">Home</a></li>
-    <li><a href="<rootFolder />/php/primi.php">Primi Piatti</a></li>
-    <li><a href="<rootFolder />/php/secondi.php">Secondi Piatti</a></li>
-    <li><a href="<rootFolder />/php/dolci.php">Dolci</a></li>
-    <li>Ricerca</li>
+<ul id="elementi_header">
+<div id="menu">
+    <li id="home"><a href="<rootFolder />/index.php">Home</a></li>
+    <li id="primi"><a href="<rootFolder />/php/primi.php">Primi Piatti</a></li>
+    <li id="secondi"><a href="<rootFolder />/php/secondi.php">Secondi Piatti</a></li>
+    <li id="dolci"><a href="<rootFolder />/php/dolci.php">Dolci</a></li>
+</div>
+    <li id="barra_ricerca">Ricerca</li>
 </ul>
