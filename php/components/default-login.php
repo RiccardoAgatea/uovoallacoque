f386b5df833af1a66bef61de7b208c9762a25f6f
ecaterina-b <ecaterinabosinceanu@gmail.com>
2020-03-22 14:23:01 +0100

    sistemato css home (da finire)

--- php/components/default-login.php
+++ php/components/default-login.php
@@ -1,4 +1,4 @@
-<ul>
-    <li><a href="<rootFolder />/php/login.php">Accedi</a></li>
-    <li><a href="<rootFolder />/php/signup.php">Registrati</a></li>
+<ul id="lista_accesso">
+    <li id="accedi_scritta"><a href="<rootFolder />/php/login.php">Accedi</a></li>
+    <li id="registrati_scritta"><a href="<rootFolder />/php/signup.php">Registrati</a></li>
 </ul>
