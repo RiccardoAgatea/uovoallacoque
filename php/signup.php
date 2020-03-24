5753be60ce61e19a4953767b81f93f802046f068
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-03-24 11:34:31 +0100

    Modificato setTitle

--- php/signup.php
+++ php/signup.php
@@ -3,7 +3,7 @@ require_once "./template-handler.php";
 
 $handler = new TemplateHandler("..", "xhtml");
 
-$handler->setTitle("Uovo alla Coque");
+$handler->setTitle("Registrati | Uovo alla Coque");
 $handler->setDescription("");
 $handler->setKeywords("");
 $handler->setAuthor("");
