a2df2241dfa4177b28b6fe0dae209db3836e75b4
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-04-22 16:07:22 +0200

    abbiamo un menu ad hambuger (parziale)

--- php/components/default-nav.php
+++ php/components/default-nav.php
@@ -1,10 +1,21 @@
 <ul id="elementi_header">
-<div id="menu">
+    <div id="menu">
+        <li id="home"><a href="<rootFolder />/index.php">Home</a></li>
+        <li id="primi"><a href="<rootFolder />/php/elenco.php?id=1">Primi Piatti</a></li>
+        <li id="secondi"><a href="<rootFolder />/php/elenco.php?id=2">Secondi Piatti</a></li>
+        <li id="dolci"><a href="<rootFolder />/php/elenco.php?id=3">Dolci</a></li>
+    </div>
+        <li id="barra_ricerca">Ricerca</li>
+</ul>
+
+<span id="apriNav" onclick="openSideNav()">&#9776; Apri menu</span>
+<ul id="mySidenav" class="sidenav">
+    <li id="chiudiNav"><a href="javascript:void(0)" class="closebtn" onclick="closeSideNav()">&times;</a></li>
+    
     <li id="home"><a href="<rootFolder />/index.php">Home</a></li>
     <li id="primi"><a href="<rootFolder />/php/elenco.php?id=1">Primi Piatti</a></li>
     <li id="secondi"><a href="<rootFolder />/php/elenco.php?id=2">Secondi Piatti</a></li>
     <li id="dolci"><a href="<rootFolder />/php/elenco.php?id=3">Dolci</a></li>
-</div>
-    <li id="barra_ricerca">Ricerca</li>
-
+<!-- <li id="barra_ricerca">Ricerca</li> -->
 </ul>
+
