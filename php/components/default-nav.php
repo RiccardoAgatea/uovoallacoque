f7ac041d37ec7a520e2eba6ad084e6a09550bca5
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-04-22 18:54:20 +0200

    messo il tasto menu a destra

--- php/components/default-nav.php
+++ php/components/default-nav.php
@@ -1,3 +1,5 @@
+<span id="apriNav" onclick="openSideNav()">&#9776; </span>
+
 <ul id="elementi_header">
     <div id="menu">
         <li id="home"><a href="<rootFolder />/index.php">Home</a></li>
@@ -8,7 +10,7 @@
         <li id="barra_ricerca">Ricerca</li>
 </ul>
 
-<span id="apriNav" onclick="openSideNav()">&#9776; Apri menu</span>
+
 <ul id="mySidenav" class="sidenav">
     <li id="chiudiNav"><a href="javascript:void(0)" class="closebtn" onclick="closeSideNav()">&times;</a></li>
     
@@ -16,6 +18,6 @@
     <li id="primi"><a href="<rootFolder />/php/elenco.php?id=1">Primi Piatti</a></li>
     <li id="secondi"><a href="<rootFolder />/php/elenco.php?id=2">Secondi Piatti</a></li>
     <li id="dolci"><a href="<rootFolder />/php/elenco.php?id=3">Dolci</a></li>
-<!-- <li id="barra_ricerca">Ricerca</li> -->
+<!-- accedi e registrati? -->
 </ul>
 
