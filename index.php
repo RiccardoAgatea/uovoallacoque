6ebc592644b7e59c60cf730448ecc921dd885e02
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-04-23 17:38:04 +0200

    elimina dead code

--- index.php
+++ index.php
@@ -31,11 +31,10 @@ $handler->setBreadcrumb(
 
 $content = file_get_contents(__DIR__ . "/php/components/home-content.php");
 
-$content = preg_replace("(<top.*Placeholder />)", "", $content);
 $risultato1 = piattoMigliore(1);
 $risultato2 = piattoMigliore(2);
 $risultato3 = piattoMigliore(3);
-$content = str_replace(array("<topPrimoPlaceholder />", "<topSecondoPlaceholder />", "<topDolcePlaceholder />"), array($risultato1, $risultato2, $risultato3), file_get_contents(__DIR__ . "/php/components/home-content.php"));
+$content = str_replace(array("<topPrimoPlaceholder />", "<topSecondoPlaceholder />", "<topDolcePlaceholder />"), array($risultato1, $risultato2, $risultato3), $content);
 
 $handler->setContent($content);
 
