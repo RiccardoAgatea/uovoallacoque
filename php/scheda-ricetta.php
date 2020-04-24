67031714fe1807d9b9a064170bd0a8c6b8695de4
Rebecca <rebecca.schiavon@studenti.unipd.it>
2020-04-24 15:04:07 +0200

    corretto risultato

--- php/scheda-ricetta.php
+++ php/scheda-ricetta.php
@@ -2,5 +2,14 @@
 
 function schedaRicetta(string $immagine, string $nome, string $difficolta, string $tempo, string $voto, string $link)
 {
-    return "<img class=\"elenco-immagine\" src=\"$immagine\" alt = \"immagine di $nome\" /><h2 class=elenco-titolo>$nome</h2><ul class=\"elenco-attributi\"><li> Difficolt&agrave;: $difficolta</li><li>Tempo: $tempo</li><li>Voto medio: $voto &frasl; 5</li></ul><a href=\"$link\">Apri</a>";
+    return "<img class=\"elenco-immagine\" src=\"$immagine\" alt = \"immagine di $nome\" />
+    <div class=elenco-testo>
+        <h2 class=elenco-titolo>$nome</h2>
+        <ul class=\"elenco-attributi\">
+            <li> Difficolt&agrave;: $difficolta</li>
+            <li>Tempo: $tempo</li>
+            <li>Voto medio: $voto &frasl; 5</li>
+        </ul>
+        <a href=\"$link\">Apri</a>
+    </div>";
 }
