cdfb6e449ac760f20d5c2099941fe1bbb5e7d974
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-04-25 11:58:02 +0200

    messo il nav nell header

--- php/components/default-nav.php
+++ php/components/default-nav.php
@@ -2,62 +2,62 @@
       onclick="openSideNav()">&#9776; </span>
 
 <ul id="menu">
-       <li id="home" class="elementoMenu">
-              <a href="<rootFolder />/index.php">
-                     Home
-              </a>
-       </li>
-       <li id="primi" class="elementoMenu">
-              <a href="<rootFolder />/php/elenco.php?id=1">
-                     Primi Piatti
-              </a>
-       </li>
-       <li id="secondi" class="elementoMenu">
-              <a href="<rootFolder />/php/elenco.php?id=2">
-                     Secondi Piatti
-              </a>
-       </li>
-       <li id="dolci" class="elementoMenu">
-              <a href="<rootFolder />/php/elenco.php?id=3">
-                     Dolci
-              </a>
-       </li>
+	<li id="home" class="elementoMenu">
+		<a href="<rootFolder />/index.php">
+			Home
+		</a>
+	</li>
+	<li id="primi" class="elementoMenu">
+		<a href="<rootFolder />/php/elenco.php?id=1">
+			Primi Piatti
+		</a>
+	</li>
+	<li id="secondi" class="elementoMenu">
+		<a href="<rootFolder />/php/elenco.php?id=2">
+			Secondi Piatti
+		</a>
+	</li>
+	<li id="dolci" class="elementoMenu">
+		<a href="<rootFolder />/php/elenco.php?id=3">
+			Dolci
+		</a>
+	</li>
 </ul>
 <div id="barra_ricerca">
-       <form method="get"
-             action="<rootFolder />/php/elenco.php?id=0">
-              <label for="termine_ricerca">Cerca:</label>
-              <input type="text"
-                     id="termine_ricerca"
-                     class="barra-cerca"
-                     name="termine_ricerca" />
-              <input type="image"
-                     id="immagine-lente"
-                     src="<rootFolder />/img/icone/lente-ingrandimento.png"
-                     alt="cerca" />
-       </form>
+	<form method="get"
+	      action="<rootFolder />/php/elenco.php?id=0">
+		<label for="termine_ricerca">Cerca:</label>
+		<input type="text"
+			id="termine_ricerca"
+			class="barra-cerca"
+			name="termine_ricerca" />
+		<input type="image"
+			id="immagine-lente"
+			src="<rootFolder />/img/icone/lente-ingrandimento.png"
+			alt="cerca" />
+	</form>
 </div>
 
 
 <ul id="mySidenav"
     class="sidenav">
-       <li id="chiudiNav" class ="sidenavElemento"><a href="javascript:void(0)"
-                 class="closebtn"
-                 onclick="closeSideNav()">&times;</a></li>
+	<li id="chiudiNav" class ="sidenavElemento"><a href="javascript:void(0)"
+		   class="closebtn"
+		   onclick="closeSideNav()">&times;</a></li>
 
-       <li id="home" class ="sidenavElemento"><a href="<rootFolder />/index.php">Home</a></li>
-       <li id="primi" class ="sidenavElemento"><a href="<rootFolder />/php/elenco.php?id=1">Primi
-                     Piatti</a></li>
-       <li id="secondi" class ="sidenavElemento"><a
-                 href="<rootFolder />/php/elenco.php?id=2">Secondi
-                     Piatti</a>
-       </li>
-       <li id="dolci" class ="sidenavElemento"><a
-                 href="<rootFolder />/php/elenco.php?id=3">Dolci</a>
-       </li>
-       <li id="accedi_scritta" class ="sidenavElemento"><a
-                 href="<rootFolder />/php/login.php">Accedi</a></li>
-       <li id="registrati_scritta" class ="sidenavElemento"><a
-                 href="<rootFolder />/php/signup.php">Registrati</a>
-       </li>
+	<li id="home" class ="sidenavElemento"><a href="<rootFolder />/index.php">Home</a></li>
+	<li id="primi" class ="sidenavElemento"><a href="<rootFolder />/php/elenco.php?id=1">Primi
+			Piatti</a></li>
+	<li id="secondi" class ="sidenavElemento"><a
+		   href="<rootFolder />/php/elenco.php?id=2">Secondi
+			Piatti</a>
+	</li>
+	<li id="dolci" class ="sidenavElemento"><a
+		   href="<rootFolder />/php/elenco.php?id=3">Dolci</a>
+	</li>
+	<li id="accedi_scritta" class ="sidenavElemento"><a
+		   href="<rootFolder />/php/login.php">Accedi</a></li>
+	<li id="registrati_scritta" class ="sidenavElemento"><a
+		   href="<rootFolder />/php/signup.php">Registrati</a>
+	</li>
 </ul>
--- php/components/xhtml-template.php
+++ php/components/xhtml-template.php
@@ -1,107 +1,109 @@
 <!DOCTYPE html
-          PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	    PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"
-      xml:lang="it"
-      lang="it">
+	xml:lang="it"
+	lang="it">
 
-      <head>
-            <title>
-                  <titlePlaceholder />
-            </title>
-            <meta name="viewport"
-                  content="width=device-width, initial-scale=1.0" />
-            <meta name="title"
-                  content="<titlePlaceholder />" />
-            <meta name="description"
-                  content="<descriptionPlaceholder />" />
-            <meta name="keywords"
-                  content="<keywordsPlaceholder />" />
-            <meta name="language"
-                  content="italian it" />
-            <meta name="author"
-                  content="<authorPlaceholder />" />
-            <meta http-equiv="Content-type"
-                  content="text/html; charset=utf-8" />
-            <link rel="icon"
-                  href="<rootFolder />/img/logo/logo-head.jpg"
-                  type="image/x-icon" />
-            <link rel="stylesheet"
-                  media="screen"
-                  href="<rootFolder />/styles/screen.css"
-                  type="text/css" />
-            <link rel="stylesheet"
-                  media="print"
-                  href="<rootFolder />/prove/print.css"
-                  type="text/css" />
-            <script src="<rootFolder />/js/script.js"></script>
-            <link rel="stylesheet"
-                  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-      </head>
+	<head>
+		<title>
+			<titlePlaceholder />
+		</title>
+		<meta name="viewport"
+			content="width=device-width, initial-scale=1.0" />
+		<meta name="title"
+			content="<titlePlaceholder />" />
+		<meta name="description"
+			content="<descriptionPlaceholder />" />
+		<meta name="keywords"
+			content="<keywordsPlaceholder />" />
+		<meta name="language"
+			content="italian it" />
+		<meta name="author"
+			content="<authorPlaceholder />" />
+		<meta http-equiv="Content-type"
+			content="text/html; charset=utf-8" />
+		<link rel="icon"
+			href="<rootFolder />/img/logo/logo-head.jpg"
+			type="image/x-icon" />
+		<link rel="stylesheet"
+			media="screen"
+			href="<rootFolder />/styles/screen.css"
+			type="text/css" />
+		<link rel="stylesheet"
+			media="print"
+			href="<rootFolder />/prove/print.css"
+			type="text/css" />
+		<script src="<rootFolder />/js/script.js"></script>
+		<link rel="stylesheet"
+			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+	</head>
 
-      <body>
-            <div id="header">
-                  <img id="immagine-logo"
-                       src="<rootFolder />/img/logo/coque_scritta_long.png"
-                       alt="Uovo alla coque" />
-            </div>
-            <div id="nav">
-                  <navPlaceholder />
-                  <loginPlaceholder />
-            </div>
-            <breadcrumbPlaceholder />
-            <contentPlaceholder />
-            <div id="footer">
-                  <p id="messaggio-footer-social"
-                     class="messaggio-footer">
-                        Seguici sui social per più ricette e novità!
-                  </p>
+	<body>
+		<div id="header">
+			<img id="immagine-logo"
+			     src="<rootFolder />/img/logo/coque_scritta_long.png"
+			     alt="Uovo alla coque" />
+			<div id="nav">
+				<navPlaceholder />
+				<loginPlaceholder />
+			</div>
+			<breadcrumbPlaceholder />
+		</div>
+		
+		<contentPlaceholder />
+		
+		<div id="footer">
+			<p id="messaggio-footer-social"
+			   class="messaggio-footer">
+				Seguici sui social per più ricette e novità!
+			</p>
 
-                  <ul id="logo-social">
-                        <li class="social">
-                              <a href="https://www.facebook.com"
-                                 class="fa fa-facebook-square"
-                                 Title="Facebook"></a>
-                        </li>
+			<ul id="logo-social">
+				<li class="social">
+					<a href="https://www.facebook.com"
+					   class="fa fa-facebook-square"
+					   Title="Facebook"></a>
+				</li>
 
-                        <li class="social">
-                              <a href="https://www.instagram.com"
-                                 class="fa fa-instagram"
-                                 Title="Instagram"></a>
-                        </li>
+				<li class="social">
+					<a href="https://www.instagram.com"
+					   class="fa fa-instagram"
+					   Title="Instagram"></a>
+				</li>
 
-                        <li class="social">
-                              <a href="https://www.youtube.com"
-                                 class="fa fa-youtube"
-                                 Title="youtube"></a>
-                        </li>
-                  </ul>
-                  <p id="messaggio-footer-copyright"
-                     class="messaggio-footer">
-                        Copyright Uovo alla coque 2020 - Tutti i diritti
-                        riservati ©
-                  </p>
+				<li class="social">
+					<a href="https://www.youtube.com"
+					   class="fa fa-youtube"
+					   Title="youtube"></a>
+				</li>
+			</ul>
+			<p id="messaggio-footer-copyright"
+			   class="messaggio-footer">
+				Copyright Uovo alla coque 2020 - Tutti i diritti
+				riservati ©
+			</p>
 
-                  <!-- <ul id="nomi-contatti">
-                <li>Agatea Riccardo</li>
-                <li>Bosinceanu Ecaterina</li>
-                <li>Righetto Sara</li>
-                <li>Schiavon Rebecca</li>
-            </ul> -->
-                  <ul id="immagine-footer">
-                        <li class="logo-footer">
-                              <img id="immagine-html"
-                                   src="<rootFolder />/img/icone/validXHTML.png"
-                                   alt="HTML valido" />
-                        </li>
-                        <li class="logo-footer">
-                              <img id="immagine-css"
-                                   src="<rootFolder />/img/icone/validCSS.png"
-                                   alt="CSS valido" />
-                        </li>
-                  </ul>
+			<!-- <ul id="nomi-contatti">
+		    <li>Agatea Riccardo</li>
+		    <li>Bosinceanu Ecaterina</li>
+		    <li>Righetto Sara</li>
+		    <li>Schiavon Rebecca</li>
+		</ul> -->
+			<ul id="immagine-footer">
+				<li class="logo-footer">
+					<img id="immagine-html"
+					     src="<rootFolder />/img/icone/validXHTML.png"
+					     alt="HTML valido" />
+				</li>
+				<li class="logo-footer">
+					<img id="immagine-css"
+					     src="<rootFolder />/img/icone/validCSS.png"
+					     alt="CSS valido" />
+				</li>
+			</ul>
 
 
-            </div>
-      </body>
+		</div>
+	</body>
 
 </html>
