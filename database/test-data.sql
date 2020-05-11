80a1bb0c738b632e94f3c1d544ffd45e5e2f8fa3
Rebecca <rebecca.schiavon@studenti.unipd.it>
2020-05-11 17:21:43 +0200

    update database con ingredienti e procedimenti

--- database/test-data.sql
+++ database/test-data.sql
@@ -1,53 +1,77 @@
-INSERT INTO ricette (nome, difficolta, tempo, img, portata)
+INSERT INTO ricette (nome, difficolta, tempo, img, portata, ingredienti, procedimento)
 VALUES
   (
     'Carbonara',
     2,
     15,
     "<rootFolder />/img/ricette/default.png",
-    1
+    1,
+    "480g pasta all\'uovo, 200g guanciale, 5 uova, 120g pecorino romano, q.b. pepe nero, 2 pizzichi sale grosso",
+    "Metti a bollire l’acqua per gli spaghetti con due pizzichi di sale grosso. Gratta il pecorino non fino, ma piuttosto grossolano.
+    Taglia il guanciale a listarelle sottili e mettilo a sfrigolare come si dice a Roma, ovvero a soffriggere dolcemente in una padella antiaderente gi&agrave; calda. Non serve aggiungere l’olio, il guanciale soffrigger&agrave; nel suo stesso grasso, che una volta diventato trasparente &egrave; pronto. Spegni la padella e lascia tutto da parte.
+    All’interno di una scodella rompi le uova, unisci una parte di pecorino (due cucchiai rasi) e aggiusta di pepe, quindi sbatti con una forchetta. Una volta cotti gli spaghetti, scolali con un forchettone direttamente nella padella a fuoco spento, magari unendo anche un mestolo scarso di acqua di cottura della pasta. Unisci subito le uova condite alla pasta e amalgama velocemente. La fiamma deve essere spenta, anzi ancora meglio se sposti la padella su un altro fornello, altrimenti con l’eccessivo calore si rischia di far diventare l’uovo una frittatina, mentre invece deve restare assolutamente cremoso (non liquido mi raccomando). Baster&agrave; il calore stesso degli spaghetti appena scolati ad amalgamare perfettamente il tutto."
   ),
   (
     'Omelette',
     3,
     5,
     "<rootFolder />/img/ricette/default.png",
-    2
+    2,
+    "2 uova grandi, 30g latte intero, q.b. sale fino, 15g olio extravergine d'oliva",
+    "Per preparare le omelette come prima cosa rompete le uova in una ciotola. Aggiungete il latte e un pizzico di sale. Poi sbattete le uova utilizzando una frusta, in modo da amalgamarle insieme al latte. Saranno pronte quando sollevando la frusta il composto verr&agrave; gi&ugrave; a filo. Scaldate un tegame sul fuoco e aggiungete un filo d'olio extravergine d'oliva. Non appena sar&agrave; caldo versate il composto di uova all'interno; per capire se l'olio &egrave; sufficientemente caldo provate a versarne prima qualche goccia. Cuocete l'omelette a fiamma moderata: con una cottura lenta l'uovo rester&agrave; pi&ugrave; morbido. La parte esterna dovr&agrave; risultare dorata ma non troppo, non appena inizier&agrave; a rapprendersi sui bordi coprite con un coperchio. Se la temperatura dovesse sembrarvi eccessiva spostate la padella dal fuoco per qualche secondo, mantenendo sempre il coperchio. Roteate la padella delicatamente per evitare che l'omelette resti sullo stesso punto per troppo tempo. Non appena l'omelette sar&agrave; morbida in superficie (ovvero bavosa) mentre sollevando il bordo con una spatola, il sotto risulter&agrave; ben dorato ripiegate l'omelette a mezzaluna, attendete pochi secondi ancora in padella e servite la vostra omelette ben calda. Semplice o accompagnata da verdure di stagione, sar&agrave; un ottimo secondo piatto. Con bacon croccante invece sar&agrave; perfetta per una colazione continentale!"
   ),
   (
     'Uovo sodo',
     1,
     8,
     "<rootFolder />/img/ricette/default.png",
-    2
+    2, 
+    "1 uovo, q.b. aceto bianco",
+    "Immergete o fate immergere delicatamente l'uovo o le uova da cuocere in acqua fredda o a temperatura ambiente (così &egrave; pi&ugrave; facile calcolare il tempo di cottura) e iniziate a contare i minuti a partire dall'ebollizione: bastano 7 minuti se volete il tuorlo cremoso, servono 8-9 minuti se al contrario vi piace compatto. Potete aggiungere all'acqua, che dovr&agrave; completamente sommergere le uova, 1 o pi&ugrave; cucchiai di aceto bianco: far&agrave; velocemente rassodare gli albumi ne caso dovessero formarsi piccole incrinature nel guscio. Scolate l'uovo sodo, passatelo immediatamente sotto l'acqua fredda per fermare la cottura quindi picchiettatelo con il dorso di un cucchiaio o di un coltello per romperne delicatamente il guscio. Mettete l'uovo sodo picchiettato in una ciotola di acqua fredda per qualche minuto, in modo che il guscio, pellicola compresa, si stacchi perfettamente dall'albume e possiate toglierlo con le mani velocemente senza problemi."
+
   ),
   (
     'Crema pasticcera',
     3,
     15,
     "<rootFolder />/img/ricette/default.png",
-    3
+    3,
+    "500g latte intero, 130g zucchero, 7 tuorli, 40g amido di riso, 2 bacelli di vaniglia, 1/2 scorza di limone",
+    "Per preparare la crema pasticcera come prima cosa inserite in freezer una ciotola di vetro. A questo punto ricavate i semi dalla vaniglia 1 e tagliate i baccelli in pezzi. Versate il latte in un tegame e aggiungete sia i semi che i baccelli della vaniglia. 
+    Ricavate dal limone la scorza, prelevando solo la parte gialla, e unitela nel tegame. Accendete il fuoco e scaldate il tutto portando a bollore e mescolando di tanto in tanto. In un altro tegame versate i tuorli, lo zucchero e l'amido di riso. Utilizzando un frustino morbido mescolate per ottenere una consistenza cremosa e liscia. 
+    Non appena il latte sfiorer&agrave; il bollore, trasferitelo all'interno dei tuorli in 3 volte filtrandolo con un colino e mescolando sempre con la frusta. Trasferite di nuovo sul fuoco e mescolate in continuazione fino a che non si sar&agrave; addensata. 
+    Per raffreddarla velocemente riprendete la ciotola dal freezer e versate la crema all'interno. Mescolate con una frusta molto velocemente, fino a che non avrete portato la crema a 50°, al di sotto del punto di cottura. Dovrete ottenere una crema liscia e molto lucida. A questo punto la crema pasticcera &egrave; pronta, potrete utilizzarla o conservarla in frigorifero coprendola con pellicola."
   ),
    (
     'Crema inglese',
     2,
     15,
     "<rootFolder />/img/ricette/default.png",
-    3
+    3,
+    "330g latte intero, 170g panna fresca liquida, 8 tuorli, 1 bacello di vaniglia, 1 scorza di limone, 100g zucchero, 1 pizzico di sale",
+    "Per preparare la crema inglese come prima cosa incidete una bacca di vaniglia, prelevatene i semi 1 e teneteli da parte. Versate poi in un pentolino latte e panna, aggiungete il baccello privato dei semi e la scorza grattugiata di un limone. Ponete sul fuoco e portate il latte a sfiorare il bollore. Trasferite i tuorli leggermente sbattuti in un altro pentolino, aggiungete i semi della vaniglia e lo zucchero. Mescolate delicatamente con una spatola cercando di non incorporare aria e unite anche il sale. 
+    Mescolate nuovamente, sempre molto lentamente, sino ad ottenere una crema omogenea. Non appena latte e panna inizierano a bollire filtrateli direttamente nel pentolino con il composto dei tuorli, in modo da trattenere anche il baccello. Mescolate sempre con la spatola e portate nuovamente la crema sul fuoco. Continuate a mescolare lentamente per evitare sempre di incorporare aria, fino a che non avrete raggiunto una temperatura di 82°. La cosa pi&ugrave; importante &egrave; quella di non superare gli 85°. Se non disponete di un termometro da cucina baster&agrave; immergervi un cucchiaino, se estraendolo la crema veler&agrave; il suo dorso significa che &egrave; pronta. Filtratela nuovamente in una ciotola pulita, in modo da bloccare la cottura. 
+    Per accelerare il raffreddamento, ponete la ciotola all'interno di un'altra ciotola pi&ugrave; grande dove avrete posto del ghiaccio. Non appena non sar&agrave; pi&ugrave; bollente coprite con la pellicola per alimenti a contatto e lasciate raffreddare completamente in frigorifero, prima di servirla e gustarla."
   ),
   (
-    'Uovo all occhio di bue',
+    'Uovo all\'occhio di bue',
     1,
     5,
     "<rootFolder />/img/ricette/default.png",
-    2
+    2,
+    "1 uovo, q.b. sale, q.b. pepe, 1 cucchiaio di olio extravergine d'oliva",
+    "Sgusciare l'uovo avendo cura di non rompere il tuorlo, dunque raccoglierlo in una ciotola. Nel frattempo, scaldare moltissimo una padella in pietra o antiaderente: quando &egrave; rovente, ungerla con un foglio di carta assorbente intinto in poco olio extravergine d'oliva. Versare l'uovo, abbassare la fiamma e cuocerlo per un paio di minuti, avendo cura di evitare la formazione della crosticina marrone ai lati degll'albume e di assicurare un tuorlo lucido e brillante (non si dovr&agrave; ottenere una consistenza troppo dura). Quando l'uovo sono pronte, farle scivolare dalla padella e servire ultimando con sale e pepe, insieme ad un contorno di verdure a piacere."
   ),
    (
     'Uovo strapazzato',
     1,
     7,
     "<rootFolder />/img/ricette/default.png",
-    2
+    2,
+    "4 uova, 30g di burro, q.b. pepe nero, q.b. sale",
+    "Rompete le uova in una ciotola e sbattetele brevemente con una forchetta per amalgamare tuorli e albume. Tagliate il burro a dadini e unitelo alle uova con una presa di sale.  
+    Scaldate pochissimo una padella o, ancora meglio, posizionatela in una padella pi&ugrave; grande contenente acqua calda in modo da effettuare una cottura a bagnomaria. Aggiungete le uova e mescolate lentamente con un cucchiaio di legno, fino a che il composto inizia a rapprendersi.
+    Proseguite la cottura, sempre mescolando, fino a che le uova diventano opache e risultano cotte, ma non asciutte. Servitele subito nel loro pentolino, ben calde, e, se vi piace, spolverizzate con una macinata di pepe."
   );
 
 INSERT INTO utenti (email, passw, nickname)
