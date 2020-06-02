bdbbca00e554e6a2ccad92c732c4fcc9e53a1abd
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-06-02 17:03:22 +0200

    tolto commenti

--- php/handle-login.php
+++ php/handle-login.php
@@ -22,30 +22,3 @@ if ($_SESSION["error"] != "") {
     header("Location: ./utente.php");
     exit;
 }
-
-
-
-
-// try {
-//     $user = new User($_POST['email']);
-
-//     if ($user->getPassword() != $_POST['password']) {
-//         exit;
-//         throw new Exception("Wrong password", 1);
-//     } else {
-//         $_SESSION["logged"] = true;
-//         $_SESSION['user'] = $user;
-//         $_SESSION["wrong-login"] = false;
-//         $_SESSION["email"] = "";
-//         $_SESSION["password"] = "";
-//         header("Location: ./utente.php");
-//         exit;
-//     }
-// } catch (Exception $e) {
-//     $_SESSION["logged"] = false;
-//     $_SESSION["wrong-login"] = true;
-//     $_SESSION["email"] = $_POST["email"];
-//     $_SESSION["password"] = $_POST["password"];
-//     header("Location: ./login.php");
-//     exit;
-// }
