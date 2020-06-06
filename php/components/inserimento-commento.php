d3d891684bd09b73a12ff4b32da93403dec70f9a
Rebecca <rebecca.schiavon@studenti.unipd.it>
2020-06-06 14:50:09 +0200

    fix validazione

--- php/components/inserimento-commento.php
+++ php/components/inserimento-commento.php
@@ -1,7 +1,7 @@
 <div id="inserimento-commento">
     <h3>Inserisci un nuovo commento</h3>
     <form id="commento-form" method="post" action="<rootFolder />/php/handle-commenti.php?ricetta=<ricettaPlaceholder />">
-    	<fieldset class=fieldset-noborder>
+    	<fieldset class="fieldset-noborder">
 	        <label for="scrivi-commento">
 	            Scrivi il tuo commento
 	        </label>
