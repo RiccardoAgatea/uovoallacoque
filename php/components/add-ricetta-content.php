69a0f302a371f32e38bee5e637ad5c7a382e71be
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-06-09 17:25:57 +0200

    #76 field per keywords

--- php/components/add-ricetta-content.php
+++ php/components/add-ricetta-content.php
@@ -1,106 +1,117 @@
 <div id="content">
-       <h1>Aggiungi ricetta</h1>
-       <p class= "add-edit-ricetta-info">Si invita l'amministratore ad aggiungere eventuali parole straniere con la seguente sintassi: [lingua=parola]. Esempio: [fr=<span xml:lang="fr">coque</span>]</p>
-       <p>I campi contrassegnati con * sono obbligatori.</p>
-       <form action="<rootFolder />/php/handle-add-ricetta.php"
-             method="post"
-             id="form-add" 
-             enctype="multipart/form-data">
-              <fieldset class="form-fieldset">
-                     <legend class="legend">
-                            Inserisci i dati della nuova ricetta
-                     </legend>
-                     <ul class="form-container">
-                            <li class="form-element">
-                                   <label class="form-label"
-                                          for="add-nome"> Nome della ricetta*: </label>
-                                   <input class="barra-input"
-                                          id="add-nome"
-                                          type="text"
-                                          name="nome"
-                                          value = "<nomePlaceholder />" />
-                                   <span id="add-nome-message"
-                                         class="warning"> <errorNomePlaceholder /> </span>
-                            </li>
-                            <li class="form-element">
-                                   <label for="add-immagine"> Immagine:</label>
-                                   <input class="barra-input"
-                                          type="file"
-                                          id="add-immagine"
-                                          name="immagine"
-                                          value = "<imgPlaceholder />" />
-                                   <span id="add-immagine-message"
-                                          class="warning"> <errorImgPlaceholder /> </span>
-                            </li>
-				                    <li> 
-                                   <span> Tipo della ricetta*: </span>
-                                   <ul> 
-                                          <li class="item-radio-container">
-                                                 <label for="primo">Primo</label>  
-                                                 <input class="radio-ricetta" type="radio" id="primo" name="tipo" value="1"/>
-                                                 
-                                          </li>
-                                          <li class="item-radio-container">   
-                                                 <label for="secondo">Secondo</label> 
-                                                 <input class="radio-ricetta" type="radio" id="secondo" name="tipo" value="2"/>
-                                                 
-                                          </li>
-                                          <li class="item-radio-container">   
-                                                 <label for="dolce">Dolce</label>
-                                                 <input class="radio-ricetta" type="radio" id="dolce" name="tipo" value="3"/>
-                                                 
-                                          </li>
-                                   </ul>
-                                   <span id="add-tipo-message"
-                                         class="warning"> <errorTipoPlaceholder /> </span>
-                            </li>
-                            <li class="form-element">
-                                   <label class="form-label"
-                                          for="add-difficolta"> Difficolt&agrave; della ricetta (da 1 a 5)*: </label> 
-                                   <input class="barra-input"
-                                          id="add-difficolta"
-                                          type="text"
-                                          name="difficolta"
-                                          value = "<difficoltaPlaceholder />" />
-                                   <span id="add-difficolta-message"
-                                         class="warning"> <errorDifficoltaPlaceholder /> </span>
-                            </li>
-                            <li class="form-element">
-                                   <label class="form-label"
-                                          for="add-tempo"> Tempo (in minuti)*: </label>
-                                   <input class="barra-input"
-                                          id="add-tempo"
-                                          type="text"
-                                          name="tempo"
-                                          value = "<tempoPlaceholder />" />
-                                   <span id="add-tempo-message"
-                                         class="warning"> <errorTempoPlaceholder /> </span>
-                            </li>
-			                     <li class="form-element">
-                                   <label class="form-label"
-                                          for="add-ingredienti"> Ingredienti (separati da virgola)*: </label>
-                                   <textarea rows="" cols=""
-                                          class="barra-input"
-                                          id="add-ingredienti"
-                                          name="ingredienti"><ingredientiPlaceholder /></textarea>
-                            </li>
-			                       <li class="form-element">
-                                   <label class="form-label"
-                                          for="add-procedura"> Procedura*: </label>
-                                   <textarea rows="" cols=""
-                                          class="barra-input"
-                                          id="add-procedura"
-                                          name="procedura"><proceduraPlaceholder /></textarea>
-                            </li>
-                            <li class="form-element">
-                                   <input class="submit"
-                                          type="submit"
-                                          value="Aggiungi" /> 
+  <h1>Aggiungi ricetta</h1>
+  <p class= "add-edit-ricetta-info">Si invita l'amministratore ad aggiungere eventuali parole straniere con la seguente sintassi: [lingua=parola]. Esempio: [fr=<span xml:lang="fr">coque</span>]</p>
+  <p>I campi contrassegnati con * sono obbligatori.</p>
+  <form action="<rootFolder />/php/handle-add-ricetta.php"
+        method="post"
+        id="form-add" 
+        enctype="multipart/form-data">
+    <fieldset class="form-fieldset">
+      <legend class="legend">
+        Inserisci i dati della nuova ricetta
+      </legend>
+      <ul class="form-container">
+        <li class="form-element">
+          <label class="form-label"
+            for="add-nome"> Nome della ricetta*: </label>
+          <input class="barra-input"
+            id="add-nome"
+            type="text"
+            name="nome"
+            value = "<nomePlaceholder />" />
+          <span id="add-nome-message"
+                class="warning"> <errorNomePlaceholder /> </span>
+        </li>
+        <li class="form-element">
+          <label for="add-immagine"> Immagine:</label>
+          <input class="barra-input"
+            type="file"
+            id="add-immagine"
+            name="immagine"
+            value = "<imgPlaceholder />" />
+          <span id="add-immagine-message"
+            class="warning"> <errorImgPlaceholder /> </span>
+        </li>
+           <li> 
+          <span> Tipo della ricetta*: </span>
+          <ul> 
+            <li class="item-radio-container">
+              <label for="primo">Primo</label>  
+              <input class="radio-ricetta" type="radio" id="primo" name="tipo" value="1"/>
+              
+            </li>
+            <li class="item-radio-container">   
+              <label for="secondo">Secondo</label> 
+              <input class="radio-ricetta" type="radio" id="secondo" name="tipo" value="2"/>
+              
+            </li>
+            <li class="item-radio-container">   
+              <label for="dolce">Dolce</label>
+              <input class="radio-ricetta" type="radio" id="dolce" name="tipo" value="3"/>
+              
+            </li>
+          </ul>
+          <span id="add-tipo-message"
+                class="warning"> <errorTipoPlaceholder /> </span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="add-difficolta"> Difficolt&agrave; della ricetta (da 1 a 5)*: </label> 
+          <input class="barra-input"
+            id="add-difficolta"
+            type="text"
+            name="difficolta"
+            value = "<difficoltaPlaceholder />" />
+          <span id="add-difficolta-message"
+                class="warning"> <errorDifficoltaPlaceholder /> </span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="add-tempo"> Tempo (in minuti)*: </label>
+          <input class="barra-input"
+            id="add-tempo"
+            type="text"
+            name="tempo"
+            value = "<tempoPlaceholder />" />
+          <span id="add-tempo-message"
+                class="warning"> <errorTempoPlaceholder /> </span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="add-keyword"> Keywords: </label>
+          <input class="barra-input"
+            id="add-keyword"
+            type="text"
+            name="keyword"
+            value = "<keywordPlaceholder />" />
+          <span id="add-keyword-message"
+                class="warning"> <errorKeywordsPlaceholder /> </span>
+        </li>
+            <li class="form-element">
+          <label class="form-label"
+            for="add-ingredienti"> Ingredienti (separati da virgola)*: </label>
+          <textarea rows="" cols=""
+            class="barra-input"
+            id="add-ingredienti"
+            name="ingredienti"><ingredientiPlaceholder /></textarea>
+        </li>
+          <li class="form-element">
+          <label class="form-label"
+            for="add-procedura"> Procedura*: </label>
+          <textarea rows="" cols=""
+            class="barra-input"
+            id="add-procedura"
+            name="procedura"><proceduraPlaceholder /></textarea>
+        </li>
+        <li class="form-element">
+          <input class="submit"
+            type="submit"
+            value="Aggiungi" /> 
 
-                            </li>
-                     </ul>
-              </fieldset>
-       </form>
-       <annullaPlaceholder />
-       <backToTopPlaceholder />
+        </li>
+      </ul>
+    </fieldset>
+  </form>
+  <annullaPlaceholder />
+  <backToTopPlaceholder />
 </div>
--- php/components/edit-ricetta-content.php
+++ php/components/edit-ricetta-content.php
@@ -1,109 +1,120 @@
 <div id="content">
-       <h1>Modifica ricetta </h1>
-       <p class= "add-edit-ricetta-info">Si invita l'amministratore ad aggiungere eventuali parole straniere con la seguente sintassi: [lingua=parola]. Esempio: [fr=<span xml:lang="fr">coque</span>]</p>
-       <p>I campi contrassegnati con * sono obbligatori.</p>
-       <form action="<rootFolder />/php/handle-edit-ricetta.php?id=<idPlaceholder/>&amp;pagina=<paginaPlaceholder/>"
-             method="post"
-             id="form-edit"
-             enctype="multipart/form-data">
-              <fieldset class="form-fieldset">
-                     <legend class="legend">
-                            Modifica i dati della ricetta
-                     </legend>
-                     <ul class="form-container">
-                            <li class="form-element">
-                                   <label class="form-label"
-                                          for="edit-nome"> Modifica nome della ricetta*: </label>
-                                   <input class="barra-input"
-                                          id="edit-nome"
-                                          type="text"
-                                          name="nome"
-                                          value="<nomeRicettaPlaceholder/>" />
-                                   <span id="edit-nome-message"
-                                         class="warning"> <errorNomePlaceholder /> </span>
-                            </li>
-                            <li class="form-element">
-                                   <label for="edit-immagine"> Modifica Immagine:</label>
-                                   <input class="barra-input"
-                                          type="file"
-                                          id="edit-immagine"
-                                          name="immagine"
-                                          value="<imgSrcPlaceholder/>" />
-                                   <span id="edit-immagine-message"
-                                          class="warning"> <errorImgPlaceholder /></span>
-                            </li>
-				                    <li> 
-                                   <span> Modifica tipo della ricetta*: </span>
-                                   <ul> 
-                                          <li class="item-radio-container">
-                                                 <label for="primo">Primo</label>  
-                                                 <input class="radio-ricetta" type="radio" id="primo" name="tipo" value="1"/>
-                                                 
-                                          </li>
-                                          <li class="item-radio-container">   
-                                                 <label for="secondo">Secondo</label> 
-                                                 <input class="radio-ricetta" type="radio" id="secondo" name="tipo" value="2"/>
-                                                 
-                                          </li>
-                                          <li class="item-radio-container">   
-                                                 <label for="dolce">Dolce</label>
-                                                 <input class="radio-ricetta" type="radio" id="dolce" name="tipo" value="3"/>
-                                                 
-                                          </li>
-                                   </ul>
-                                   <span id="edit-tipo-message"
-                                         class="warning"> <errorTipoPlaceholder /> </span>
-                            </li>
-                            <li class="form-element">
-                                   <label class="form-label"
-                                          for="edit-difficolta"> Modifica difficolt&agrave; della ricetta (da 1 a 5)*: </label> 
-                                   <input class="barra-input"
-                                          id="edit-difficolta"
-                                          type="text"
-                                          name="difficolta"
-                                          value="<difficoltaPlaceholder/>" />
-                                   <span id="edit-difficolta-message"
-                                         class="warning"> <errorDifficoltaPlaceholder />  </span>
-                            </li>
-                            <li class="form-element">
-                                   <label class="form-label"
-                                          for="edit-tempo"> Modifica tempo(in minuti)*: </label>
-                                   <input class="barra-input"
-                                          id="edit-tempo"
-                                          type="text"
-                                          name="tempo"
-                                          value="<tempoPlaceholder/>" />
-                                   <span id="edit-tempo-message"
-                                         class="warning"> <errorTempoPlaceholder /></span>
-                            </li>
-			       <li class="form-element">
-                                   <label class="form-label"
-                                          for="edit-ingredienti"> Modifica ingredienti (separati da virgola)*: </label>
-                                   <textarea rows="" cols=""
-                                          class="barra-input"
-                                          id="edit-ingredienti"
-                                          name="ingredienti"><ingredientiPlaceholder/></textarea>
-                                   <span id="edit-ingredienti-message"
-                                         class="warning"> </span>
-                            </li>
-			       <li class="form-element">
-                                   <label class="form-label"
-                                          for="edit-procedura"> Modifica procedura*: </label>
-                                   <textarea rows="" cols=""
-                                          class="barra-input"
-                                          id="edit-procedura"
-                                          name="procedura"><proceduraPlaceholder/></textarea>
-                                   <span id="edit-procedura-message"
-                                         class="warning"> </span>
-                            </li>
-                            <li class="form-element">
-                                   <input class="submit"
-                                          type="submit"
-                                          value="Salva modifiche" />
-                            </li>
-                     </ul>
-              </fieldset>
-       </form>
-       <annullaPlaceholder />
-       <backToTopPlaceholder />
+  <h1>Modifica ricetta </h1>
+  <p class= "add-edit-ricetta-info">Si invita l'amministratore ad aggiungere eventuali parole straniere con la seguente sintassi: [lingua=parola]. Esempio: [fr=<span xml:lang="fr">coque</span>]</p>
+  <p>I campi contrassegnati con * sono obbligatori.</p>
+  <form action="<rootFolder />/php/handle-edit-ricetta.php?id=<idPlaceholder/>&amp;pagina=<paginaPlaceholder/>"
+        method="post"
+        id="form-edit"
+        enctype="multipart/form-data">
+    <fieldset class="form-fieldset">
+      <legend class="legend">
+        Modifica i dati della ricetta
+      </legend>
+      <ul class="form-container">
+        <li class="form-element">
+          <label class="form-label"
+            for="edit-nome"> Modifica nome della ricetta*: </label>
+          <input class="barra-input"
+            id="edit-nome"
+            type="text"
+            name="nome"
+            value="<nomeRicettaPlaceholder/>" />
+          <span id="edit-nome-message"
+                class="warning"> <errorNomePlaceholder /> </span>
+        </li>
+        <li class="form-element">
+          <label for="edit-immagine"> Modifica Immagine:</label>
+          <input class="barra-input"
+            type="file"
+            id="edit-immagine"
+            name="immagine"
+            value="<imgSrcPlaceholder/>" />
+          <span id="edit-immagine-message"
+            class="warning"> <errorImgPlaceholder /></span>
+        </li>
+                  <li> 
+          <span> Modifica tipo della ricetta*: </span>
+          <ul> 
+            <li class="item-radio-container">
+              <label for="primo">Primo</label>  
+              <input class="radio-ricetta" type="radio" id="primo" name="tipo" value="1"/>
+              
+            </li>
+            <li class="item-radio-container">   
+              <label for="secondo">Secondo</label> 
+              <input class="radio-ricetta" type="radio" id="secondo" name="tipo" value="2"/>
+              
+            </li>
+            <li class="item-radio-container">   
+              <label for="dolce">Dolce</label>
+              <input class="radio-ricetta" type="radio" id="dolce" name="tipo" value="3"/>
+              
+            </li>
+          </ul>
+          <span id="edit-tipo-message"
+                class="warning"> <errorTipoPlaceholder /> </span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="edit-difficolta"> Modifica difficolt&agrave; della ricetta (da 1 a 5)*: </label> 
+          <input class="barra-input"
+            id="edit-difficolta"
+            type="text"
+            name="difficolta"
+            value="<difficoltaPlaceholder/>" />
+          <span id="edit-difficolta-message"
+                class="warning"> <errorDifficoltaPlaceholder />  </span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="edit-tempo"> Modifica tempo(in minuti)*: </label>
+          <input class="barra-input"
+            id="edit-tempo"
+            type="text"
+            name="tempo"
+            value="<tempoPlaceholder/>" />
+          <span id="edit-tempo-message"
+                class="warning"> <errorTempoPlaceholder /></span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="edit-keyword"> Keywords: </label>
+          <input class="barra-input"
+            id="edit-keyword"
+            type="text"
+            name="keyword"
+            value = "<keywordPlaceholder />" />
+          <span id="edit-keyword-message"
+                class="warning"> <errorKeywordsPlaceholder /> </span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="edit-ingredienti"> Modifica ingredienti (separati da virgola)*: </label>
+          <textarea rows="" cols=""
+            class="barra-input"
+            id="edit-ingredienti"
+            name="ingredienti"><ingredientiPlaceholder/></textarea>
+          <span id="edit-ingredienti-message"
+                class="warning"> </span>
+        </li>
+        <li class="form-element">
+          <label class="form-label"
+            for="edit-procedura"> Modifica procedura*: </label>
+          <textarea rows="" cols=""
+            class="barra-input"
+            id="edit-procedura"
+            name="procedura"><proceduraPlaceholder/></textarea>
+          <span id="edit-procedura-message"
+                class="warning"> </span>
+        </li>
+        <li class="form-element">
+          <input class="submit"
+            type="submit"
+            value="Salva modifiche" />
+        </li>
+      </ul>
+    </fieldset>
+  </form>
+  <annullaPlaceholder />
+  <backToTopPlaceholder />
 </div>
