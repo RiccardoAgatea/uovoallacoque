e9e3e7801db296e61da84460a219527a95c2dd2b
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-06-09 16:41:45 +0200

    6 ricette

--- php/elenco.php
+++ php/elenco.php
@@ -87,7 +87,7 @@ $handler->setNav($nav);
 $content = file_get_contents(__DIR__ . "/components/elenco-content.php");
 $content = str_replace("<categoriaPlaceholder />", $tipi[$tipo], $content);
 
-$perPagina = 9;
+$perPagina = 6;
 $pagina = (key_exists("pagina", $_GET)) ? intval($_GET['pagina']) : 1;
 $primoElemento = ($pagina - 1) * $perPagina;
 
