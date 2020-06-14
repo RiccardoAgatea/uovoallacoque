539ca88180f1888849775e207250987e213a4030
AkaiSara <sara.righetto.1@studenti.unipd.it>
2020-06-14 17:30:18 +0200

    id con nomi diversi

--- php/components/inserimento-commento.php
+++ php/components/inserimento-commento.php
@@ -1,11 +1,12 @@
 <div id="inserimento-commento">
     <h3>Inserisci un nuovo commento</h3>
-    <form id="commento-form" method="post" action="<rootFolder />/php/handle-commenti.php?ricetta=<ricettaPlaceholder />">
+    <form id="inserisci-commento-form" method="post" action="<rootFolder />/php/handle-commenti.php?ricetta=<ricettaPlaceholder />">
     	<fieldset class="fieldset-noborder">
 	        <label for="scrivi-commento">
 	            Scrivi il tuo commento
 	        </label>
 	        <textarea rows="" cols="" id="scrivi-commento" name="scrivi-commento"></textarea>
+			<span id="scrivi-commento-message" class="warning"> <errorCommentoPlaceholder /> </span>
 	        <input class="commento-submit" type="submit" value="Commenta"/>
 	    </fieldset>
 	</form>
--- php/components/modifica-commento.php
+++ php/components/modifica-commento.php
@@ -1,6 +1,6 @@
 <div id="inserimento-commento">
     <h3>Modifica il commento</h3>
-    <form id="commento-form" method="post" action="<rootFolder />/php/handle-modifica-commento.php?ricetta=<ricettaPlaceholder />&idcommento=<idCommentoPlaceholder />&pagina=<paginaPlaceholder />">
+    <form id="modifica-commento-form" method="post" action="<rootFolder />/php/handle-modifica-commento.php?ricetta=<ricettaPlaceholder />&idcommento=<idCommentoPlaceholder />&pagina=<paginaPlaceholder />">
     	<fieldset class=fieldset-noborder>
 	        <label for="commento">
 	            Modifica il tuo commento
